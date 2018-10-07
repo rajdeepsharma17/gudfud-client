d78ff6eebe97797e5aa0376dd9d85d43b7463b0b
Raj <rajdeep.sharma@intelliticks.com>
2018-10-08 02:16:26 +0530

    added Splash Screen
    implemented Firebase auth

--- lib/Navigate/navigate.dart
+++ lib/Navigate/navigate.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 
 class Navigate extends StatelessWidget {
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    print("signed in " + user.displayName);
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -17,6 +35,7 @@ class Navigate extends StatelessWidget {
           ListTile(
             title: Text('Item 1'),
             onTap: () {
+              _handleSignIn();
             },
           ),
           ListTile(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import './Menu/menu.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     FlutterStatusbarcolor.setStatusBarColor(Colors.white);
@@ -13,10 +15,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(          // Add the 3 lines from here...
+      theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: Menu(),
+      home: SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: Menu(),
+        title: Text('Welcome In SplashScreen',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0
+        ),),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: ()=>print("Flutter Egypt"),
+        loaderColor: Colors.red
+      ),
     );
   }
 }
