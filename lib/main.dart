a27cd2424e55d52ecc83a6a6d4956c96f3e88238
Raj <rajdeep.sharma@intelliticks.com>
2018-10-08 03:51:31 +0530

    added Intro Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:splashscreen/splashscreen.dart';
+import 'package:intro_views_flutter/Models/page_view_model.dart';
+import 'package:intro_views_flutter/intro_views_flutter.dart';
 
 import './Menu/menu.dart';
 
@@ -20,12 +22,13 @@ class MyApp extends StatelessWidget {
       ),
       home: SplashScreen(
         seconds: 3,
-        navigateAfterSeconds: Menu(),
+        navigateAfterSeconds: Intro(),
         title: Text('Welcome In SplashScreen',
         style: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 20.0
         ),),
+        image: Image.network('https://i.imgur.com/TyCSG9A.png'),
         backgroundColor: Colors.white,
         styleTextUnderTheLoader: new TextStyle(),
         photoSize: 100.0,
@@ -33,6 +36,95 @@ class MyApp extends StatelessWidget {
         loaderColor: Colors.red
       ),
     );
+  }
+}
+
+
+class Intro extends StatelessWidget {
+  //making list of pages needed to pass in IntroViewsFlutter constructor.
+  final pages = [
+    new PageViewModel(
+        pageColor: const Color(0xFF03A9F4),
+        iconImageAssetPath: 'assets/air-hostess.png',
+        iconColor: null,
+        bubbleBackgroundColor: null,
+        body: Text(
+          'Haselfree  booking  of  flight  tickets  with  full  refund  on  cancelation',
+        ),
+        title: Text(
+          'Flights',
+        ),
+        textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+        mainImage: Image.asset(
+          'assets/airplane.png',
+          height: 285.0,
+          width: 285.0,
+          alignment: Alignment.center,
+        )),
+    new PageViewModel(
+      pageColor: const Color(0xFF8BC34A),
+      iconImageAssetPath: 'assets/waiter.png',
+      iconColor: null,
+      bubbleBackgroundColor: null,
+      body: Text(
+        'We  work  for  the  comfort ,  enjoy  your  stay  at  our  beautiful  hotels',
+      ),
+      title: Text('Hotels'),
+      mainImage: Image.asset(
+        'assets/hotel.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+    new PageViewModel(
+      pageColor: const Color(0xFF607D8B),
+      iconImageAssetPath: 'assets/taxi-driver.png',
+      iconColor: null,
+      bubbleBackgroundColor: null,
+      body: Text(
+        'Easy  cab  booking  at  your  doorstep  with  cashless  payment  system',
+      ),
+      title: Text('Cabs'),
+      mainImage: Image.asset(
+        'assets/taxi.png',
+        height: 285.0,
+        width: 285.0,
+        alignment: Alignment.center,
+      ),
+      textStyle: TextStyle(fontFamily: 'MyFont', color: Colors.white),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'IntroViews Flutter', //title of app
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ), //ThemeData
+      home: new Builder(
+        builder: (context) => new IntroViewsFlutter(
+              pages,
+              onTapDoneButton: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                    builder: (context) => Menu(),
+                  ), //MaterialPageRoute
+                );
+              },
+              showSkipButton:
+                  true, //Whether you want to show the skip button or not.
+              pageButtonTextStyles: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ), //IntroViewsFlutter
+      ), //Builder
+    ); //Material App
   }
 }
 
