c8c3376c75607f85443fd7f005127a35f72ed6bb
Raj <rajdeep.sharma@intelliticks.com>
2018-10-07 14:51:17 +0530

    initial Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+
     return MaterialApp(
       title: 'Startup',
       debugShowCheckedModeBanner: false,
@@ -21,7 +24,7 @@ class RandomWordsState extends State<RandomWords> {
 
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
-  final TextStyle _biggerFont = const TextStyle(fontSize: 25.0);
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   void _pushSaved() {
     Navigator.of(context).push(
@@ -59,36 +62,89 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold (
       appBar: AppBar(
-        title: Text('Menu'),
-        actions: <Widget>[      // Add 3 lines from here...
-          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text('GUDFUD', style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontFamily: 'ProductSans',
+        ),),
       ),
       body: _buildSuggestions(),
     );
   }
 
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+  return Column(
+    children: <Widget>[
+      Expanded(
+        child: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return Divider();
+
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              return _buildRow(_suggestions[index]);
+            }),
+      ),
+      Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              height: 48.0,
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text('CHECKOUT', style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ),
+                    Expanded(
+                      child: Text('₹ 225', textAlign: TextAlign.end, style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),),
+                    )
+                  ],
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 12.0),              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0.0, -1.0),
+                    blurRadius: 0.5,
+                    color: Colors.black12
+                  )
+                ]
+              ),
+            ),
+          )
+        ],
+      )
+    ],
+  );
   }
 
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
     return ListTile(
+      leading: Container(
+        width: 40.0,
+        height: 40.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(image: NetworkImage('https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png'))
+        ),
+      ),
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      subtitle: Text('₹ 112'),
       trailing: new Icon(   // Add the lines from here...
         alreadySaved ? Icons.plus_one : Icons.add,
         color: alreadySaved ? Colors.red : null,
